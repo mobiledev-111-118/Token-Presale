fbfb2aaf8585980257ada7df785ecafd8ebfa4f8
mobile-dev-hybrid <cutecate2019118@gmail.com>
2021-07-10 22:08:42 +0300

    remove dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,8 @@
 require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-etherscan');
-require('dotenv');
+
+const {alchemyKey, ringkebyPrivateKey} = require("./secrets.json");
 
 task('accounts', 'Prints the list of accounts', async () => {
   const accounts = await ethers.getSigners();
@@ -14,8 +15,6 @@ task('accounts', 'Prints the list of accounts', async () => {
   }
 });
 
-const {alchemyKey, ringkebyPrivateKey} = require("./secrets.json");
-console.log(alchemyKey, ringkebyPrivateKey)
 module.exports = {
   defaultNetwork: "rinkeby",
   networks: {
@@ -23,6 +22,10 @@ module.exports = {
 			url: 'http://127.0.0.1:8545',
 		},
 		hardhat: {},
+    mainnet: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyKey}`,
+      accounts: [`0x${ringkebyPrivateKey}`]
+    },
     rinkeby: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyKey}`,
       accounts: [`0x${ringkebyPrivateKey}`]
